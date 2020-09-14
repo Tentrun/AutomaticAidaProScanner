228541c938be0a9c4607ce7eebc2be889af25c30
Tentrun <tentrun@gmail.com>
2020-09-14 17:56:10 +0300

    Main

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kabinet = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.metroCheckBox1 = new MetroFramework.Controls.MetroCheckBox();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.metroProgressSpinner1 = new MetroFramework.Controls.MetroProgressSpinner();
+            this.metroLabel7 = new MetroFramework.Controls.MetroLabel();
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kabinet
+            // 
+            this.kabinet.Location = new System.Drawing.Point(95, 76);
+            this.kabinet.Name = "kabinet";
+            this.kabinet.Size = new System.Drawing.Size(100, 20);
+            this.kabinet.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(298, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Сделать отчет";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton1.FontWeight = MetroFramework.MetroButtonWeight.Light;
+            this.metroButton1.Location = new System.Drawing.Point(221, 241);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(93, 23);
+            this.metroButton1.TabIndex = 13;
+            this.metroButton1.Text = "Самосбор";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton2.FontWeight = MetroFramework.MetroButtonWeight.Light;
+            this.metroButton2.Location = new System.Drawing.Point(122, 241);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(93, 23);
+            this.metroButton2.TabIndex = 14;
+            this.metroButton2.Text = "Нетбук";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton3.FontWeight = MetroFramework.MetroButtonWeight.Light;
+            this.metroButton3.Location = new System.Drawing.Point(23, 241);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(93, 23);
+            this.metroButton3.TabIndex = 15;
+            this.metroButton3.Text = "Ноутбук";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(30, 76);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(59, 19);
+            this.metroLabel1.TabIndex = 17;
+            this.metroLabel1.Text = "Кабинет";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.BackColor = System.Drawing.Color.Red;
+            this.metroLabel2.Location = new System.Drawing.Point(183, 200);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(0, 0);
+            this.metroLabel2.TabIndex = 18;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(30, 124);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(56, 19);
+            this.metroLabel3.TabIndex = 19;
+            this.metroLabel3.Text = "Модель";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(30, 165);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(52, 19);
+            this.metroLabel4.TabIndex = 20;
+            this.metroLabel4.Text = "Кол-во";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 123);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 21;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(95, 165);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 22;
+            // 
+            // metroCheckBox1
+            // 
+            this.metroCheckBox1.AutoSize = true;
+            this.metroCheckBox1.Location = new System.Drawing.Point(298, 97);
+            this.metroCheckBox1.Name = "metroCheckBox1";
+            this.metroCheckBox1.Size = new System.Drawing.Size(189, 15);
+            this.metroCheckBox1.TabIndex = 24;
+            this.metroCheckBox1.Text = "Тестирование новых функций";
+            this.metroCheckBox1.UseSelectable = true;
+            this.metroCheckBox1.CheckedChanged += new System.EventHandler(this.metroCheckBox1_CheckedChanged);
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.Location = new System.Drawing.Point(366, 260);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(121, 19);
+            this.metroLabel5.TabIndex = 25;
+            this.metroLabel5.Text = "github.com/tentrun";
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.AutoSize = true;
+            this.metroLabel6.Location = new System.Drawing.Point(278, 115);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(229, 19);
+            this.metroLabel6.TabIndex = 27;
+            this.metroLabel6.Text = "(не трогать эту кнопку без команды)";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.ErrorImage = global::WindowsFormsApp1.Properties.Resources.logo;
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.logo;
+            this.pictureBox1.InitialImage = global::WindowsFormsApp1.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(95, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 54);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 26;
+            this.pictureBox1.TabStop = false;
+            // 
+            // metroProgressSpinner1
+            // 
+            this.metroProgressSpinner1.EnsureVisible = false;
+            this.metroProgressSpinner1.Location = new System.Drawing.Point(329, 137);
+            this.metroProgressSpinner1.Maximum = 10;
+            this.metroProgressSpinner1.Name = "metroProgressSpinner1";
+            this.metroProgressSpinner1.Size = new System.Drawing.Size(90, 82);
+            this.metroProgressSpinner1.TabIndex = 28;
+            this.metroProgressSpinner1.UseSelectable = true;
+            this.metroProgressSpinner1.Value = 3;
+            this.metroProgressSpinner1.Click += new System.EventHandler(this.metroProgressSpinner1_Click);
+            // 
+            // metroLabel7
+            // 
+            this.metroLabel7.AutoSize = true;
+            this.metroLabel7.Location = new System.Drawing.Point(57, 210);
+            this.metroLabel7.Name = "metroLabel7";
+            this.metroLabel7.Size = new System.Drawing.Size(0, 0);
+            this.metroLabel7.TabIndex = 29;
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 23;
+            this.metroComboBox1.Items.AddRange(new object[] {
+            "MainPage",
+            "Parser"});
+            this.metroComboBox1.Location = new System.Drawing.Point(366, 225);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(121, 29);
+            this.metroComboBox1.TabIndex = 31;
+            this.metroComboBox1.UseSelectable = true;
+            this.metroComboBox1.SelectedIndexChanged += new System.EventHandler(this.metroComboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 287);
+            this.Controls.Add(this.metroComboBox1);
+            this.Controls.Add(this.metroLabel7);
+            this.Controls.Add(this.metroProgressSpinner1);
+            this.Controls.Add(this.metroLabel6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.metroLabel5);
+            this.Controls.Add(this.metroCheckBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.metroLabel4);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.kabinet);
+            this.Name = "Form1";
+            this.Text = "ИЦТО";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox kabinet;
+        private System.Windows.Forms.Button button2;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private MetroFramework.Controls.MetroProgressSpinner metroProgressSpinner1;
+        private MetroFramework.Controls.MetroLabel metroLabel7;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using MetroFramework.Properties;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+
+        string compType = null;
+        string saveto = null;
+        string infostring = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            metroProgressSpinner1.Value = 0;
+            metroProgressSpinner1.Spinning = false;
+
+            bool isElevated;
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            isElevated = principal.IsInRole(WindowsBuiltInRole.Administrator);
+
+            
+
+            if(!isElevated)
+            {
+
+                ProcessStartInfo processStartInfo = new ProcessStartInfo();
+                processStartInfo.Verb = "runas";
+                processStartInfo.FileName = Application.ExecutablePath;
+                try
+                {
+                    Process.Start(processStartInfo);
+                }
+                catch (Win32Exception)
+                {
+                    metroLabel2.Text = "Не удалось запустить от имени администратора";
+                    Application.Exit();
+                }
+            }
+            else
+            {
+
+            }
+
+
+
+        }
+
+        private void generateSaveto()
+        {
+            saveto = string.Format("кабинет_{0}_{1}_{2}_{3}.html", kabinet.Text, compType, textBox1.Text, textBox2.Text);
+        }
+
+
+
+        private async Task startAida()
+        {
+
+
+            Process aidabin = new Process();
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "aida64.exe";
+            infostring = string.Format("/R {0} /TEXT /LANGRU /CUSTOM format.rpf /SAFE", saveto);
+            info.Arguments = infostring;
+            info.UseShellExecute = false;
+            aidabin.StartInfo = info;
+            button2.Enabled = false;
+            aidabin.Start();
+            aidabin.WaitForExit(); //После закрытия приложения можно топать дальше.
+            closeApp();
+        }
+        private void closeApp()
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!metroCheckBox1.Checked)
+            {
+
+                kabinet.Enabled = false;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                metroButton1.Enabled = false;
+                metroButton2.Enabled = false;
+                metroButton3.Enabled = false;
+                button2.Enabled = false;
+                metroCheckBox1.Enabled = false;
+
+                metroProgressSpinner1.Value = 3;
+                metroProgressSpinner1.Spinning = true;
+
+
+                generateSaveto();
+                startAida();
+            }
+            else
+            {
+
+                    string result;
+                    string req = "SELECT Name FROM Win32_MotherboardDevice";
+                    ManagementObjectSearcher searcher = new ManagementObjectSearcher(req);
+                    //MessageBox.Show(saveto);
+
+                    foreach (ManagementObject obj in searcher.Get())
+                    {
+                        result = obj["Name"].ToString().Trim();
+                        MessageBox.Show(result);
+                    }
+            }
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            compType = "Ноутбук";
+            metroLabel7.Text = "Вы выбрали : " + compType;
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            compType = "Нетбук";
+            metroLabel7.Text = "Вы выбрали : " + compType;
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            compType = "Самосбор";
+            metroLabel7.Text = "Вы выбрали : " + compType;
+        }
+
+        private void metroCheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroProgressSpinner1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(metroComboBox1.Text == "Parser")
+            {
+
+            }
+        }
+    }
+}
